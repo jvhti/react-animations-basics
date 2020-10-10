c3f1edafedcfc1e7ceaa248678a782f9565932cb
jvhti <jvhti@hotmail.com>
2020-10-10 11:29:00 -0300

    Using react-transition-group/Transition to animate the div

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -20,7 +21,18 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className="Button" onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
         <br/>
-        {this.state.showBlock ? <div style={{backgroundColor: 'red', width: 100, height: 100, margin: 'auto'}}></div> : null}
+        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+          {state => (
+              <div style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1
+              }}></div>
+          )}
+        </Transition>
         {this.state.modalIsOpen ? <Modal show closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop show/> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
